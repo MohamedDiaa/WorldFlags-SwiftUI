82f0843492672e7681e226ddaee9b79afe71418d
Mohamed Alwakil <mohamed.diaa@qvik.fi>
2023-01-09 17:11:36 +0100

    Initial Commit

---
+++ WorldFlags/WorldFlagsApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorldFlagsApp.swift
+//  WorldFlags
+//
+//  Created by Mohamed Alwakil on 2023-01-09.
+//
+
+import SwiftUI
+
+@main
+struct WorldFlagsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
