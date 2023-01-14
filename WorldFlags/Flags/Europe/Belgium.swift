30e991850ed1bb48dd016be32a7a0dcf31ce9fdc
Mohamed Alwakil <mohamed.diaa@qvik.fi>
2023-01-14 18:40:07 +0100

    add Belgium Flag

---
+++ WorldFlags/Flags/Europe/Belgium.swift
@@ -0,0 +1,26 @@
+//
+//  Belgium.swift
+//  WorldFlags
+//
+//  Created by Mohamed Alwakil on 2023-01-14.
+//
+
+import SwiftUI
+
+struct Belgium: View {
+    var body: some View {
+
+        HStack(spacing: 0) {
+
+            Color.black
+            Color.yellow
+            Color.red
+        }
+    }
+}
+
+struct Belgium_Previews: PreviewProvider {
+    static var previews: some View {
+        Belgium()
+    }
+}
