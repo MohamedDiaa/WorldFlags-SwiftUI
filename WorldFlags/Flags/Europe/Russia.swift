64f322161af7a2b34e13df9cdac92d8b90683ae0
Mohamed Alwakil <mohamed.diaa@qvik.fi>
2023-01-16 10:59:37 +0100

    add Russian flag

---
+++ WorldFlags/Flags/Europe/Russia.swift
@@ -0,0 +1,25 @@
+//
+//  Russia.swift
+//  WorldFlags
+//
+//  Created by Mohamed Alwakil on 2023-01-16.
+//
+
+import SwiftUI
+
+struct Russia: View {
+    var body: some View {
+
+        VStack(spacing: 0) {
+            Color.white
+            Color.blue
+            Color.red
+        }
+    }
+}
+
+struct Russia_Previews: PreviewProvider {
+    static var previews: some View {
+        Russia()
+    }
+}
