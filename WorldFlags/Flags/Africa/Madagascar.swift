4812ab6446c07cb666636c50359fc4c1fed43c39
Mohamed Alwakil <736736+MohamedDiaa@users.noreply.github.com>
2023-01-18 18:54:16 +0100

    fix Madagascar colors

--- WorldFlags/Flags/Africa/Madagascar.swift
+++ WorldFlags/Flags/Africa/Madagascar.swift
@@ -9,7 +9,8 @@ import SwiftUI
 
 struct Madagascar: View {
     var body: some View {
-
+        let red = Color(red: 0.9, green: 0.31, blue: 0.26)
+        let green = Color(red: 0.23, green: 0.51, blue: 0.26)
         GeometryReader { geometry in
 
             HStack(spacing: 0) {
@@ -17,8 +18,8 @@ struct Madagascar: View {
                 Color.white
                     .frame(width: geometry.size.width * 2.0 / 5.0)
                 VStack(spacing: 0) {
-                    Color.red
-                    Color.green
+                    red
+                    green
                 }
             }
         }
