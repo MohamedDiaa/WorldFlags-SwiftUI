cee20361912291e8492d3703a301625f0fdaab6e
Mohamed Alwakil <736736+MohamedDiaa@users.noreply.github.com>
2023-01-20 13:48:47 +0100

    Add Estonia flag

---
+++ WorldFlags/Flags/Europe/Nordic/Estonia.swift
@@ -0,0 +1,26 @@
+//
+//  Estonia.swift
+//  WorldFlags
+//
+//  Created by Mohamed Alwakil on 2023-01-20.
+//
+
+import SwiftUI
+
+struct Estonia: View {
+    var body: some View {
+        let blue = Color(red: 0.19, green: 0.44, blue: 0.79)
+
+        VStack(spacing: 0) {
+            blue
+            Color.black
+            Color.white
+        }
+    }
+}
+
+struct Estonia_Previews: PreviewProvider {
+    static var previews: some View {
+        Estonia()
+    }
+}
