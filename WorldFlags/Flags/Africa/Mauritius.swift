7bf85dfbcfa2f29927ed1d1effc3a1c316b54dc5
Mohamed Alwakil <736736+MohamedDiaa@users.noreply.github.com>
2023-01-24 18:30:13 +0100

    add Mauritius flag

---
+++ WorldFlags/Flags/Africa/Mauritius.swift
@@ -0,0 +1,26 @@
+//
+//  Mauritius.swift
+//  WorldFlags
+//
+//  Created by Mohamed Alwakil on 2023-01-24.
+//
+
+import SwiftUI
+
+struct Mauritius: View {
+    var body: some View {
+        
+        VStack(spacing: 0) {
+            Color(red: 0.85, green: 0.23, blue: 0.25)
+            Color(red: 0.08, green: 0.10, blue: 0.41)
+            Color(red: 0.97, green: 0.85, blue: 0.29)
+            Color(red: 0.29, green: 0.64, blue: 0.35)
+        }
+    }
+}
+
+struct Mauritius_Previews: PreviewProvider {
+    static var previews: some View {
+        Mauritius()
+    }
+}
