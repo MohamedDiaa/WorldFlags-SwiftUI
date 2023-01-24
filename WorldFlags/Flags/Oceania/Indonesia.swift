6eaa29d77d580ae5878724125a2163bd35c906d5
Mohamed Alwakil <736736+MohamedDiaa@users.noreply.github.com>
2023-01-24 16:23:04 +0100

    add flag of Indonesia

---
+++ WorldFlags/Flags/Oceania/Indonesia.swift
@@ -0,0 +1,24 @@
+//
+//  Indonesia.swift
+//  WorldFlags
+//
+//  Created by Mohamed Alwakil on 2023-01-24.
+//
+
+import SwiftUI
+
+struct Indonesia: View {
+    var body: some View {
+        let red = Color(red: 0.92, green: 0.2, blue: 0.14)
+        VStack {
+            red
+            Color.white
+        }
+    }
+}
+
+struct Indonesia_Previews: PreviewProvider {
+    static var previews: some View {
+        Indonesia()
+    }
+}
