d932ceeaffc6303130dec00ede85f0c31b530987
Mohamed Alwakil <736736+MohamedDiaa@users.noreply.github.com>
2023-01-26 17:38:04 +0100

    add Yemen flag

---
+++ WorldFlags/Flags/Mena/Yemen.swift
@@ -0,0 +1,26 @@
+//
+//  Yemen.swift
+//  WorldFlags
+//
+//  Created by Mohamed Alwakil on 2023-01-26.
+//
+
+import SwiftUI
+
+struct Yemen: View {
+    var body: some View {
+        let red = Color(red: 0.75, green: 0.16, blue: 0.17)
+
+        VStack {
+            red
+            Color.white
+            Color.black
+        }
+    }
+}
+
+struct Yemen_Previews: PreviewProvider {
+    static var previews: some View {
+        Yemen()
+    }
+}
